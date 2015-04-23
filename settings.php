1deae5935a7fc149fbd5c3fbca936ece45170b18
neswill1 <nenadkonatar@live.ac.me>
2015-04-23 11:01:38 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,23 @@
+<?php
+$adresa[0]=" 6345 Naples Blvd. Naples, FL 34109 "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="Naples, FL 34109";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="BOULEVARD PLAZA";
+$telephone[0]=2395935700;// Broj koji se poziva na dugme CALL US
+
+$adresa[1]="2314 Immokalee Road Naples, FL 34110"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[1]="Naples, FL 34110";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[1]="GREEN TREE PLAZA";
+$telephone[1]=2395130942;// Broj koji se poziva na dugme CALL US
+
+$adresa[2]="6345 Naples Blvd. Naples, FL 34109"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[2]="Naples, FL 34109";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[2]="THE COLLECTION";
+$telephone[2]=2395935700;// Broj koji se poziva na dugme CALL US
+
+$companynumber=3;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
